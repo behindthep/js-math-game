c522bf2ae175f4cf5645c4847511b2e3be77e844
Alexey Kolvakh <alex.kolvah@mail.ru>
2025-07-16 17:55:56 +0300

    chore

--- js/script.js
+++ js/script.js
@@ -5,11 +5,14 @@ const check = document.querySelector('.check')
 const getRandomInRange = (min = 0, max = 9) => {
   return Math.floor(min + Math.random() * (max - min + 1))
 }
+
 let firstNumber;
 let secondNumber;
+
 const generateExample = () => {
   firstNumber = getRandomInRange()
   secondNumber = getRandomInRange()
+
   template.textContent = `${firstNumber} * ${secondNumber}`
 }
 
@@ -17,11 +20,12 @@ generateExample()
 
 check.addEventListener('click', () => {
   if (firstNumber * secondNumber === Number(input.value)) {
-    alert('Поздравляю, это верный ответ')
+    alert('Это верный ответ!')
     generateExample()
   } else {
-    alert('Извините, ответ неверный, попробуйте еще раз')
+    alert('Неверно, попробуйте еще раз.')
   }
+
   input.value = ''
   input.focus()
 })
